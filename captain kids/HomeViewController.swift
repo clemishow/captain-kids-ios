4c8454070930d410530157e1d6a9b157b48611de
clemishow <clement.saulnier@hetic.net>
2017-12-20 23:07:49 +0100

    [GET] Data home page if there is a picking

--- captain kids/HomeViewController.swift
+++ captain kids/HomeViewController.swift
@@ -13,16 +13,33 @@ import FirebaseDatabase
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var displayNameLabel: UILabel!
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let user = Auth.auth().currentUser
+        ref = Database.database().reference()
+        
+        
+        
         if user != nil {
             print("user sign in")
             print(user!)
             print(user!.email!)
             displayNameLabel.text = "Bonjour " + user!.displayName!
+            ref.child("picking").child(user!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+                print(snapshot.value)
+                if let result = snapshot.children.allObjects as? [DataSnapshot] {
+                    for child in result {
+                        var orderID = child.key as! String
+                        print(orderID)
+                    }
+                }
+               
+            }) { (error) in
+                print(error.localizedDescription)
+            }
         } else {
             print("no user sign in")
         }
