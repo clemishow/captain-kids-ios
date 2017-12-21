b6d85a268a56a0c57286701feefcb81e575379b7
clemishow <clement.saulnier@hetic.net>
2017-12-21 23:54:09 +0100

    [UPDATE] Launch screen

---
+++ LauchScreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LauchScreenViewController.swift
+//  captain kids
+//
+//  Created by Dev on 21/12/2017.
+//  Copyright © 2017 Hetic. All rights reserved.
+//
+
+import UIKit
+
+class LauchScreenViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
